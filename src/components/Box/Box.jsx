39ff79e380e96949634b39baa7f64f51a6f37a7b
Vebster <vebster.khm@gmail.com>
2022-06-29 01:09:37 +0300

    Delete Box.jsx

--- src/components/Box/Box.jsx
+++
@@ -1,5 +0,0 @@
-import styled from '@emotion/styled';
-
-import { space, layout, color, flexbox, border } from 'styled-system';
-
-export const Box = styled('div')(space, layout, color, flexbox, border);
